a74d5a6af59ece8cbc3420a0c6211e9de2a48d86
Omar Alam <omeralam7@gmail.com>
2019-01-19 20:54:54 -0500

    Commit new sensor code

--- src/frc/team4015/sensors/rangeFinder.java
+++ src/frc/team4015/sensors/rangeFinder.java
@@ -1,4 +1,32 @@
 package frc.team4015.sensors;
 
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+/***
+ * @Author: Omar
+ * THIS CLASS IS TO GET A THEORETICAL ULTRASONIC SENSOR WORKING THAT WILL RETURN THE RANGE IN MILLIMETERS.
+ */
 public class rangeFinder {
+
+    Ultrasonic sensor;
+
+    public rangeFinder(int channel,int channel2){
+        sensor = new Ultrasonic(channel,channel2); //TODO: NEED TO FIGURE OUT INPUT AND OUTPUT CHANNELS BASED ON WHERE ELECTRICAL TEAM PLUGS THE SENSORS IN
+        sensor.setAutomaticMode(true);
+    }
+
+
+    /**
+     * This method can be called to return the range being measured from a sensor.
+     * @return Range in millimeters measured from the sensor
+     */
+    public double getRange(){
+        double rangeMeasured = sensor.getRangeMM();
+        if (sensor.isRangeValid()){
+            return rangeMeasured;
+        }else{
+            return 0.0; // INVALID RANGE MEASUREMENT. NOTHING CAN BE 0M CLOSE TO THE SENSOR
+        }
+    }
+
 }
--- src/frc/team4015/sentience/aimingCV.java
+++ src/frc/team4015/sentience/aimingCV.java
@@ -1,4 +1,30 @@
 package frc.team4015.sentience;
+import frc.team4015.Robot;
+import frc.team4015.subsystems.*;
+import org.opencv.core.Mat;
+import org.opencv.features2d.*;
+import org.opencv.*;
 
+/***
+ * @Author: Omar Alam
+ * THIS CLASS WILL HANDLE THE COMPUTER VISION OPERATIONS FOR THE FRONT FACING CAMERA. MOSTLY USED TO ALIGN THE ROBOT FOR
+ * SHOOTING I BELIEVE.
+ * TODO: Get Image Data into objects
+ * TODO: Figure out what this camera will actually do
+ * TODO: Actually program image processing
+ */
 public class aimingCV {
+
+    private Mat imageData;
+
+    aimingCV(){
+        Robot.cvFrontCamera.
+    }
+
+    public void aim(){
+
+        imageData = Robot.cvFrontCamera.
+    }
+
+
 }
